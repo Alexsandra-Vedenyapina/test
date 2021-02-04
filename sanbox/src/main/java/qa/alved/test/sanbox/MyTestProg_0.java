a3fa52b4dfd3346c30d0aca8ec5dbe06e7ef4447
Alexsandra-Vedenyapina <78475568+Alexsandra-Vedenyapina@users.noreply.github.com>
2021-02-04 15:55:46 +0300

    Добавлено пару функций

--- sanbox/src/main/java/qa/alved/test/sanbox/MyTestProg_0.java
+++ sanbox/src/main/java/qa/alved/test/sanbox/MyTestProg_0.java
@@ -3,6 +3,27 @@ package qa.alved.test.sanbox;
 public class MyTestProg_0 {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("user");
+    hello("Alex");
+
+    double l = 5;
+    System.out.println("Площадь квадрата: " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of a Rectangle with sides " + a + " and " + b + " = " + area(a, b));
+
+  }
+
+  public static void  hello(String nameHello) {
+    System.out.println("Hello, " + nameHello + "!");
+  }
+
+  public static double area(double len ) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a*b;
   }
 }
