533834bc1cadd373ee362bf59b6ba9689ecc411b
Alexsandra-Vedenyapina <78475568+Alexsandra-Vedenyapina@users.noreply.github.com>
2021-02-04 14:30:16 +0300

    Изменены настройки структуры кода

--- sanbox/src/main/java/qa/alved/test/sanbox/MyTestProg_0.java
+++ sanbox/src/main/java/qa/alved/test/sanbox/MyTestProg_0.java
@@ -2,7 +2,7 @@ package qa.alved.test.sanbox;
 
 public class MyTestProg_0 {
 
-public static void main(String[] args) {
-System.out.println("Hello, World!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
