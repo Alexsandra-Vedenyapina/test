38ed6fd6e53e6b9f34f7bb38111f3052c78d7ebd
Alexsandra-Vedenyapina <78475568+Alexsandra-Vedenyapina@users.noreply.github.com>
2021-02-05 13:05:27 +0300

    добавление расчета дистанции между точками

--- sanbox/src/main/java/qa/alved/test/sanbox/MyTestProg_0.java
+++ sanbox/src/main/java/qa/alved/test/sanbox/MyTestProg_0.java
@@ -12,15 +12,24 @@ public class MyTestProg_0 {
     Rectangle r = new Rectangle(4,6);
         System.out.println("Area of a Rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
 
+    double x1 = 1;
+    double y1 = 1;
+    double x2 = 3;
+    double y2 = 4;
+    System.out.println("Дистанция между точкой P1 (" + x1 + " , " + y1 + ") " + " и точкой P2 (" + x1 + " , " + y1 + ") " + distance(x1,y1,x2,y2));
+
   }
 
   public static void  hello(String Hello) {
    System.out.println("Hello, " + Hello + "!");
   }
 
-  public static double distance(Point p1, Point p2) {
-    double sqrt = Math.sqrt(double a, double b);
-    Math.sqrt
+  public static double distance(
+          double x1,
+          double y1,
+          double x2,
+          double y2) {
+    return Math.sqrt((y2 - y1) *  (y2 - y1) + (x2 - x1) *  (x2 - x1));
   }
 
 
